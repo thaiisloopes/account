8f0ec0139542a7c8dc91dcf7dc2aeaabfeeb8a6b
thaislopes <tlnloopes@gmail.com>
2024-10-28 20:07:38 -0300

    add mockk dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("io.mockk:mockk:1.13.12")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
